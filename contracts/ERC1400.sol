4af3ca0f6f395c86d0cfd08b2bd71ee9712e7560
komus-Israel <komolehinIsrael@gmail.com>
2022-01-23 16:16:17 +0100

    name, symbol and decimal are initialized in the constructor

--- contracts/ERC1400.sol
+++ contracts/ERC1400.sol
@@ -4,5 +4,17 @@ pragma solidity 0.8.10;
 
 
 contract ERC1400 {
-    
+
+    string public name;
+    string public symbol;
+    uint256 public decimal;
+
+    constructor (string memory _name, string memory _symbol, uint256 _decimal) {
+
+        name = _name;
+        symbol = _symbol;
+        decimal = _decimal;
+
+    }
+
 }
--- test/ERC1400.test.js
+++ test/ERC1400.test.js
@@ -8,9 +8,12 @@ require("chai")
 contract('ERC1400', ()=>{
 
     let erc1400
+    let name = "Tangl"
+    let symbol = "TAN"
+    let decimal = 18
 
     beforeEach( async()=>{
-        erc1400 = await ERC1400.new()
+        erc1400 = await ERC1400.new(name, symbol, decimal)
     })
 
     //  deployment test
@@ -23,6 +26,34 @@ contract('ERC1400', ()=>{
     
             contractAddress.should.be.not.equal("", "it has a contract address")
     
+        })
+
+    })
+
+    // variable initialization when contract loads
+
+    describe("test variable initialization in constructor", ()=>{
+
+        it("has a name", async()=>{
+
+            const tokenName = await erc1400.name()
+            tokenName.should.be.equal(name, "the name was initialized at the constructor")
+
+        })
+
+        it("has a symbol", async()=>{
+
+            const tokenSymbol = await erc1400.symbol()
+            tokenSymbol.should.be.equal(symbol, "the symbol was initialized at the constructor")
+
+        })
+
+
+        it("has decimals", async()=>{
+
+            const tokenDecimal = await erc1400.decimal()
+            tokenDecimal.toString().should.be.equal(decimal.toString(), "the decimal was initialized at the constructor")
+
         })
 
     })
