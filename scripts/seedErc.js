ee28811af306f8c6f41f89a6ed7396dc897f4341
komus-Israel <komolehinIsrael@gmail.com>
2022-01-26 20:46:19 +0100

    issued tokens to self

--- scripts/seedErc.js
+++ scripts/seedErc.js
@@ -1,11 +1,22 @@
 //const { default: Web3 } = require("web3")
 
-const erc1400 = artifacts.require("./ERC1400")
+const ERC1400 = artifacts.require("./ERC1400")
 
 
 module.exports = async function(callback) {
     try {
         const accounts = await web3.eth.getAccounts()
+
+        // fetch the deployed contract
+        const erc1400 = await ERC1400.deployed()
+
+
+        const issuer = accounts[0]
+
+        // issue tokens to self
+        await erc1400.issueTokens(issuer, 10, {from: issuer})
+
+        console.log("token sent to issuer")
     } catch(err) {
         console.log(err)
     }
