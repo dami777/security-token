57910522337f8603b477c599c615ef20697c97b6
komus-Israel <komolehinIsrael@gmail.com>
2022-02-09 21:30:34 +0100

    highlighted the basic steps in a comment

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -5,6 +5,15 @@ pragma solidity 0.8.10;
 
 contract Certificate {
 
+    //  this contract is to test data signing
+    //  steps:
+
+    //  1. Have the message (structured data should be EIP 712 standard)
+    //  2. Hash the Message
+    //  3. Generate another hash of the hash message in 3
+    //  4. Sign the hashed message offchain
+    //  5. Verify the signature
+
 
     function generateMessageHash() {
 
@@ -15,7 +24,7 @@ contract Certificate {
     }
 
     function verifySignature() {
-        
+
     }
 
 
