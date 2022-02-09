0b04299e452d878b87d8d12e77c38d797ab989a0
komus-Israel <komolehinIsrael@gmail.com>
2022-02-09 21:22:36 +0100

    created file and initialized functions to generate and verify signature

---
+++ contracts/Certificate.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.8.10;
+
+
+
+
+contract Certificate {
+
+
+    function generateMessageHash() {
+
+    }
+
+    function generateHashToSign() {
+
+    }
+
+    function verifySignature() {
+        
+    }
+
+
+}
