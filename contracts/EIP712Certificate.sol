5503266a7250a5cde5b79dcd2483fe718082d390
komus-Israel <komolehinIsrael@gmail.com>
2022-02-11 21:18:01 +0100

    initialized the domain separator values

--- contracts/EIP712Certificate.sol
+++ contracts/EIP712Certificate.sol
@@ -15,5 +15,15 @@ contract EIP712 {
     string constant IDENTITY_TYPE = "Identity(address _from, address _to, uint256 _amount)";
 
 
+    // ******* Define the Domain Separator Values ****** //
+
+    uint256 chainId = 5777;
+    address verifyingContract = address(this);
+    bytes32 salt = 0x54132a91a1bafcf3d90beaad0c0d5f0bda635715da5017e515739dbb823f282d;
+    string EIP712_DOMAIN = "EIP712Domain(string name, string version, uint256 chainId, address verifyingContract, bytes32 salt)";
+
+    
+
+
 }
 
