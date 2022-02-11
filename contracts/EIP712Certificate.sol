fe8881684f1d560ac3e70ec43905ab26b4940ffe
komus-Israel <komolehinIsrael@gmail.com>
2022-02-11 21:12:24 +0100

    created a struct called identity and created a type hash for it

--- contracts/EIP712Certificate.sol
+++ contracts/EIP712Certificate.sol
@@ -4,6 +4,16 @@ contract EIP712 {
 
     //  EIP712 standard for signing a structured data
 
+    struct Identitty {
+
+        address _from;
+        address _to;
+        uint256 _amount;
+
+    }
+
+    string constant IDENTITY_TYPE = "Identity(address _from, address _to, uint256 _amount)";
+
 
 }
 
