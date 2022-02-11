50f5dacbb89148771f6cf21caba9aeef4a604e31
komus-Israel <komolehinIsrael@gmail.com>
2022-02-11 21:26:37 +0100

    created the domain separator bytes

--- contracts/EIP712Certificate.sol
+++ contracts/EIP712Certificate.sol
@@ -17,12 +17,24 @@ contract EIP712 {
 
     // ******* Define the Domain Separator Values ****** //
 
-    uint256 chainId = 5777;
+    uint256 constant chainId = 5777;
     address verifyingContract = address(this);
-    bytes32 salt = 0x54132a91a1bafcf3d90beaad0c0d5f0bda635715da5017e515739dbb823f282d;
-    string EIP712_DOMAIN = "EIP712Domain(string name, string version, uint256 chainId, address verifyingContract, bytes32 salt)";
+    bytes32 constant salt = 0x54132a91a1bafcf3d90beaad0c0d5f0bda635715da5017e515739dbb823f282d;      // an hardcoded salt value
+    string constant EIP712_DOMAIN_HASH_TYPE = "EIP712Domain(string name, string version, uint256 chainId, address verifyingContract, bytes32 salt)";
 
-    
+
+    bytes32 constant DOMAIN_SEPARATOR = keccak256(abi.encodePacked(
+
+        EIP712_DOMAIN_HASH_TYPE,
+        keccak256(bytes("Dapp Name")),
+        keccak256(bytes("1")),
+        chainId,
+        verifyingContract,
+        salt
+
+    ));
+
+
 
 
 }
