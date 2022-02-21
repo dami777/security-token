68029b56606411b501aa8badd67cfa1601b0533e
komus-Israel <komolehinIsrael@gmail.com>
2022-02-21 22:23:57 +0100

    initialized the operator controller transfer function

--- contracts/ERC1400.sol
+++ contracts/ERC1400.sol
@@ -307,6 +307,12 @@ contract ERC1400 {
        return _isControllable;
    }
 
+   function controllerTransfer(address _from, address _to, uint256 _value, bytes _data, bytes _operatorData) external {
+
+   }
+
+
+
 
 
    
