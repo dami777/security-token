d2347ef5754a9a3f19ec355867d9c91eb9122be3
komus-Israel <komolehinIsrael@gmail.com>
2022-03-02 09:26:56 +0100

    declared returnedSigner to be public

--- test/Certificate.test.js
+++ test/Certificate.test.js
@@ -50,7 +50,8 @@ contract("Certificate Data Test", ()=>{
         })
 
         it("verifies the signer", async()=>{
-            const returnedSigner = await cert.verifySignature(signature, prefixedHash)
+            await cert.verifySignature(signature, prefixedHash)
+            const returnedSigner = await cert.returnedSigner()
             //returnedSigner.should.be.equal(signer, "it verifies the signer and generates the right signer")
             console.log(returnedSigner)
         })
