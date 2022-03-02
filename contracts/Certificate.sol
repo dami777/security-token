8b30bd41027d55639678e7c7a338e683d48fa3f6
komus-Israel <komolehinIsrael@gmail.com>
2022-03-02 06:16:01 +0100

    created typed hash for the transfer struct

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -19,11 +19,14 @@ contract Certificate {
 
         Person from;
         Person to;
+        uint256 amount;
 
     }
 
 
     bytes32 private constant PERSON_TYPED_HASH = keccak256("Person(string firstName, string lastName, string location, walletAddress)");
+    bytes32 private constant TRANSFER_TYPED_HASH = keccak256("Transfer(Person from, Person to, uint256 amount)Person(string firstName, string lastName, string location, walletAddress)");
+
 
     /*function generateHash () {
 
