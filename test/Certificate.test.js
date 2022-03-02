1a0cbc5e95d348d6b4e6d3280ea51a7297595d97
komus-Israel <komolehinIsrael@gmail.com>
2022-03-02 09:24:10 +0100

    declared verifySignature to be view

--- test/Certificate.test.js
+++ test/Certificate.test.js
@@ -37,7 +37,8 @@ contract("Certificate Data Test", ()=>{
         }
 
         let prefixedHash
-
+        let signature = "0xd86a15acf203b4602a59ba476f9edac51817085e2a26e843ddc85f0c5e04bc2d104d443efdb530614a9ba3af70cb02a2b8b4c9a4157c9a45e20fd774784c21441b"
+        let signer  = "0xa3CfeF02b1D2ecB6aa51B133177Ee29764f25e31"
 
         beforeEach(async()=>{
             prefixed = await cert.hashTransfer(from, to, 100)
@@ -45,7 +46,13 @@ contract("Certificate Data Test", ()=>{
 
         it("generates the prefixed hash", async()=>{
             await prefixed.should.not.be.equal("", "it generates the prefixed hash")
-            console.log(prefixed)
+            //console.log(prefixed)
+        })
+
+        it("verifies the signer", async()=>{
+            const returnedSigner = await cert.verifySignature(signature, prefixedHash)
+            //returnedSigner.should.be.equal(signer, "it verifies the signer and generates the right signer")
+            console.log(returnedSigner)
         })
 
     })
