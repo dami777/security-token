23f6f72bfe634bd8caea6922c28d7c53d0879f1f
komus-Israel <komolehinIsrael@gmail.com>
2022-03-02 05:24:50 +0100

    test that the Certificate contract has an address

--- test/Certificate.test.js
+++ test/Certificate.test.js
@@ -1,1 +1,24 @@
 const Certificate = artifacts.require("./Certificate")
+
+require("chai")
+    .use(require("chai-as-promised"))
+    .should()
+
+contract("Certificate Data Test", ()=>{
+
+    let cert
+
+    beforeEach(async()=>{
+        cert = await Certificate.new()
+    })
+
+    describe("contract deployment", ()=>{
+
+        it("has a contract address", async()=>{
+            cert.address.should.not.be.equal("", "it has a contract address")
+        })
+
+    })
+
+
+})
