641707c1ee92a3308eda0a016d430c7b55c82f1e
komus-Israel <komolehinIsrael@gmail.com>
2022-03-10 21:07:58 +0100

    added a simple notice that gives a little information about the contract

--- contracts/HTLC.sol
+++ contracts/HTLC.sol
@@ -4,4 +4,8 @@ pragma solidity 0.8.10;
 
 contract HTLC {
 
+
+    /// @notice Contract for DVP. Seller discloses the preimage of the hash. Secret is exchanged for money over the block chain
+   
+
 }
