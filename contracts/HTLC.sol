d64ef91cb1e2e7eef2979b589bcb768e966578e6
komus-Israel <komolehinIsrael@gmail.com>
2022-03-14 22:04:18 +0100

    added dev comment to understand who the buyer and seller is

--- contracts/HTLC.sol
+++ contracts/HTLC.sol
@@ -8,6 +8,10 @@ contract HTLC {
     /// @notice Contract for DVP. Seller discloses the preimage of the hash. Secret is exchanged for money over the block chain
     /// @notice Seller is permitted to spend the funds by disclosing the preimage of a hash
     /// @notice Buyer is permitted to spend the funds after a timeout is reached, in a refund situation
-    
+    /// @dev    HTLC enable atomic swap
+    /// @dev    Buyer: entity that receives funds from seller once the seller reveals the secret
+    /// @dev    Seller: entity that contributes funds to the buyer by revealing the secret or refunds after expiration
+
+
 
 }
