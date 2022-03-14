20f089a4888bfe83158a008c6f96f62a6aadb440
komus-Israel <komolehinIsrael@gmail.com>
2022-03-14 22:00:40 +0100

    added notice comment about the buyer and seller

--- contracts/HTLC.sol
+++ contracts/HTLC.sol
@@ -6,6 +6,8 @@ contract HTLC {
 
 
     /// @notice Contract for DVP. Seller discloses the preimage of the hash. Secret is exchanged for money over the block chain
-   
+    /// @notice Seller is permitted to spend the funds by disclosing the preimage of a hash
+    /// @notice Buyer is permitted to spend the funds after a timeout is reached, in a refund situation
+    
 
 }
