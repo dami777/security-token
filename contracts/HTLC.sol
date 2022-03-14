113b0dd90a782c9eb2299ca3898584660130b5bc
komus-Israel <komolehinIsrael@gmail.com>
2022-03-14 22:07:47 +0100

    added dev comment to explain secret, secret hash and the expiration

--- contracts/HTLC.sol
+++ contracts/HTLC.sol
@@ -11,7 +11,10 @@ contract HTLC {
     /// @dev    HTLC enable atomic swap
     /// @dev    Buyer: entity that receives funds from seller once the seller reveals the secret
     /// @dev    Seller: entity that contributes funds to the buyer by revealing the secret or refunds after expiration
-
+    /// @dev    Secret: random number chosen by the seller revealed to allow the buyer to redeem the funds
+    /// @dev    Secret Hash: hash of the secret. used in the construction of the HTLC
+    /// @dev    Expiration: timestamp that determines when seller and buyer can redeem
+    
 
 
 }
