ec0d1522479f96747c61ceec71e68e1c9cdde168
komus-Israel <komolehinIsrael@gmail.com>
2022-03-16 01:12:12 +0100

    imported ierc20 in htlc20 contract and ierc1400 in the htlc1400 contract

--- contracts/HTLC1400.sol
+++ contracts/HTLC1400.sol
@@ -1,6 +1,5 @@
 pragma solidity 0.8.10;
 
-import "./utils/IERC20.sol";
 import "./utils/IERC1400.sol";
 
 
--- contracts/HTLC20.sol
+++ contracts/HTLC20.sol
@@ -1,8 +1,10 @@
 /// @title HTLC to release USDT from investor to issuer
 
+import "./utils/IERC20.sol";
+
 contract HTLC20 {
 
-    
+
 
 
 }
