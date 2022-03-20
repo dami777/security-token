8db7ca771e330ad7da952f6473b45dd825bdcf40
komus-Israel <komolehinIsrael@gmail.com>
2022-03-20 20:42:32 +0100

    fixed error in line 90. used secretHash to store order details instead of swapID hence the reason for withdrawal failure

--- contracts/HTLC1400.sol
+++ contracts/HTLC1400.sol
@@ -87,7 +87,7 @@ contract HTLC1400 {
 
         require(_swapState[_swapID] == SwapState.INVALID, "order ID exist already");
         require( _secretHash == sha256(abi.encode(_secretKey)), "the secret doen't match the hash");
-        _orderSwap[_secretHash] = OrderSwap(_recipient, _tokenValue, _expiration, _secretHash, bytes32(0), _partition, _swapID);         // save the order on the blockchain so that the target investor can make reference to it for withdrawal
+        _orderSwap[_swapID] = OrderSwap(_recipient, _tokenValue, _expiration, _secretHash, bytes32(0), _partition, _swapID);         // save the order on the blockchain so that the target investor can make reference to it for withdrawal
         ERC1400_TOKEN.operatorTransferByPartition(_partition, msg.sender, address(this), _tokenValue, "", _data);                        // the htlc contract moves tokens from the caller's wallet, i.e the issuer and deposits them in its address to be released to the expected recipient
         _swapState[_swapID] = SwapState.OPEN;                                                                                            // keep the order state OPEN till it is CLOSES or EXPIRES
         emit OpenedOrder(_recipient, _tokenValue, _expiration, _secretHash, _partition);
@@ -118,6 +118,8 @@ contract HTLC1400 {
         
 
     }
+
+    
 
    
 
