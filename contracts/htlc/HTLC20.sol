d242500efe85d40127eb2ed097ded466dd5526be
komus-Israel <komolehinIsrael@gmail.com>
2022-03-22 20:10:14 +0100

    created the struct and mappings to handle the orders and swap state

--- contracts/htlc/HTLC20.sol
+++ contracts/htlc/HTLC20.sol
@@ -1,5 +1,5 @@
 /// @title  HTLC to release USDT from investor to issuer
-/// @dev    Contract for DVP. Investor releases his payment and gets the security token in exchange.
+/// @dev    Contract for DVP. Investor funds this contract with his usdt and gets the security token in exchange.
 
 pragma solidity 0.8.10;
 
@@ -8,16 +8,34 @@ import "../utils/IERC20.sol";
 contract HTLC20 {
 
 
-    /*function fund() {
+    mapping(bytes32 => OrderSwap) private _orderSwap;      //  map the order struct to the order ID
+    mapping(bytes32 => SwapState) private _swapState;      //  to keep track of the swap state of an id
 
+
+    IERC20 ERC20_TOKEN;
+
+    struct OrderSwap {
+
+        
+        address _recipient;
+        address _issuer;
+        uint256 _tokenValue;
+        uint256 _expiration;
+        bytes32 _secretHash;
+        bytes32 _secretKey;
+        bytes32 _partition;
+        bytes32 _swapID;
+        
     }
 
-    function withDraw() {
+
+    enum SwapState {
+
+        INVALID,
+        OPEN,
+        CLOSED,
+        EXPIRED
 
     }
-
-    function refund() {
-
-    }*/
 
 }
