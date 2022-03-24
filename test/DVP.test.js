950413d50045e11159cd2143d0717e7310a8d449
komus-Israel <komolehinIsrael@gmail.com>
2022-03-24 13:52:38 +0100

    investor purchases usdt from external source and funds the order for dvp

--- test/DVP.test.js
+++ test/DVP.test.js
@@ -41,11 +41,52 @@ contract ("DVP", ([issuer, investor])=>{
 
         erc20 = await ERC20_USDT.new("US Dollars Tether", "USDT")
         htlc20 = await HTLC20.new(erc20.address)
-        
+
         erc1400 = await ERC1400.new(name, symbol, decimal, totalSupply, [classA, classB] )
         htlc1400 = await HTLC1400.new(erc1400.address)
 
         await erc1400.setController(signer)
+    })
+
+    describe("Issuer opens orders on the two htlc contracts", ()=>{
+
+        let orderID = web3.utils.asciiToHex("x23dvsdgd")
+        let openOrderHtlc20
+        let openOrderHtlc1400
+
+        let expirationHTLC20 = new Date(moment().add(1, 'days').unix()).getTime()    // expiration will be present time + 1 day
+        let expirationHTLC11400 = new Date(moment().add(2, 'days').unix()).getTime()    // expiration will be present time + 2 day
+
+        let secret = web3.utils.asciiToHex("anonymous")
+        let dataHex = web3.eth.abi.encodeParameter("bytes32", secret1)
+        let secretHash = ethers.utils.sha256(dataHex1)
+
+        beforeEach(()=>{
+
+            openOrderHtlc20 = await htlc20.openOrder(orderID, investor1, tokens(1000), expiration, secretHash, secret, {from: issuer})
+            openOrderHtlc1400 = await htlc1400.openOrder(orderID, secret1, hash1, classA, investor, tokens(5), expiration, data, {from: issuer})
+        })
+
+        describe("success", ()=>{
+            openOrderHtlc20.logs[0].event.should.be.equal("OpenOrder", "issuer opens order on the htlc 20 contract")
+            openOrderHtlc1400.logs[0].event.should.be.equal("OpenOrder", "issuer opens order on the htlc 1400 contract")
+        })
+
+        describe("fill order", async()=>{
+
+            beforeEach(()=>{
+                //  investor purchases USDT from p2p/escrow, exchanges
+                await erc20.transfer(investor1, tokens(2000))           // investor purchases usdt token from escrow/exchanges/p2p/any secondary market
+
+                // investor approves the htlc20 contract to move tokens from his wallet to fund the order
+                await erc20.approve(htlc20.address, tokens(1000), {from: investor1})  // investor approves the htlc contract to move the tokens from his wallet to fund the order
+
+                //  investor funds the order
+                await htlc20.fundOrder(orderID, {from: investor1})
+            })
+
+        })
+
     })
 
 })
