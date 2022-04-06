f530a0105615f98f67cf05ac28b57d9582c548e0
komus-Israel <komolehinisrael@gmail.com>
2022-04-06 21:02:55 +0100

    linked the GenerateEthSig to the CertificateLibrary in the migration script

--- migrations/1_migrate_erc1400.js
+++ migrations/1_migrate_erc1400.js
@@ -1,7 +1,7 @@
 const ERC1400 = artifacts.require("ERC1400");
 const HTLC1400 = artifacts.require("HTLC1400");
 const Certificate = artifacts.require("Certificate")
-const GenerateCertificate = artifacts.require("GenerateEthSignature")
+const GenerateEthSignature = artifacts.require("GenerateEthSignature")
 let classA = web3.utils.asciiToHex("CLASS A");
 let classB = web3.utils.asciiToHex("CLASS B");
 
@@ -10,8 +10,8 @@ module.exports = function (deployer) {
   deployer.deploy(Certificate).then(
     () => {
       deployer.link(Certificate, ERC1400)
-      deployer.link(Certificate, GenerateCertificate)
-      deployer.deploy(GenerateCertificate)
+      deployer.link(Certificate, GenerateEthSignature)
+      deployer.deploy(GenerateEthSignature)
       
       deployer.deploy(ERC1400, "TANGLE", "TAN", 18, 0, [classA, classB]).then(
 
