3daf2415f804943822ba0e586b8e5513de3fc821
komus-Israel <komolehinisrael@gmail.com>
2022-04-06 20:34:26 +0100

    limit the domain generator's function parameters from 5 to 1

--- contracts/utils/CertificateLibrary.sol
+++ contracts/utils/CertificateLibrary.sol
@@ -30,7 +30,7 @@ library Certificate {
         string companyName;
         uint256 chainiID;
         bytes32 salt;
-        
+
     }
 
     
@@ -45,17 +45,18 @@ library Certificate {
     /// @param chainId The chain Id where the data will be signed
     /// @param salt An hardcoded byte32. One of the security measures for the signature
 
-    function generateDomainSepartor (address verifyingContract, string memory version, string memory companyName, uint256 chainId, bytes32 salt) internal pure returns (bytes32) {
+
+    function generateDomainSepartor(DomainData memory _domainData) internal pure returns (bytes32) {
 
         bytes32 EIP712_DOMAIN_HASH_TYPE = keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract,bytes32 salt)");
 
         return keccak256(abi.encode(
             EIP712_DOMAIN_HASH_TYPE,
-            keccak256(bytes(companyName)),
-            keccak256(bytes(version)),
-            chainId,
-            verifyingContract,
-            salt
+            keccak256(bytes(_domainData.companyName)),
+            keccak256(bytes(_domainData.version)),
+            _domainData.chainId,
+            _domainData.verifyingContract,
+            _domainData.salt
         ));
     }
 
@@ -75,14 +76,15 @@ library Certificate {
     /// @dev    the hashHolder function hashes the _from and _to separately as they are different Holders entirely
     /// @return the prefixed hash
 
-    function hashTransfer(address verifyingContract, string memory version, string memory companyName, uint256 chainId, bytes32 salt, Holder memory _from, Holder memory _to, uint256 _amount) external view returns (bytes32) {
+
+    function hashTransfer(DomainData memory _domainData, Holder memory _from, Holder memory _to, uint256 _amount) external view returns (bytes32) {
         
         bytes32 TRANSFER_TYPED_HASH = keccak256("TransferData(Holder from,Holder to,uint256 amount)Holder(string firstName,string lastName,string location,address walletAddress)");
         
         return keccak256(
             abi.encodePacked(
             "\x19\x01", 
-                generateDomainSepartor(verifyingContract, version, companyName, chainId, salt),
+                generateDomainSepartor(_domainData),
                 keccak256(abi.encode(
                     TRANSFER_TYPED_HASH,
                     hashHolder(_from),
