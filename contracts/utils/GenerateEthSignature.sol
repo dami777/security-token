b4f782eb1bc41da86d4621cdc19ae99e28fee744
komus-Israel <komolehinisrael@gmail.com>
2022-04-08 14:41:38 +0100

    declared generateEthSignature function to be pure

--- contracts/utils/GenerateEthSignature.sol
+++ contracts/utils/GenerateEthSignature.sol
@@ -6,7 +6,7 @@ contract GenerateEthSignature {
 
     /// @dev    This function generates a bytes32 prefixed signature that will be used to verify the signed Typed Data Signature
 
-    function generateEthSignature(Certificate.DomainData memory _domainData, Certificate.Holder memory _from, Certificate.Holder memory _to, uint256 _amount) external view returns (bytes32) {
+    function generateEthSignature(Certificate.DomainData memory _domainData, Certificate.Holder memory _from, Certificate.Holder memory _to, uint256 _amount) external pure returns (bytes32) {
         
         return Certificate.hashTransfer(_domainData, _from, _to, _amount);
 
