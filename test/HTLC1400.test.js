5b027f1bd412fd9bcf3ffd497df94814ecb671c8
komus-Israel <komolehinisrael@gmail.com>
2022-04-26 21:53:27 +0100

    fixed error in the test of the returned investor address when chekingorder

--- test/HTLC1400.test.js
+++ test/HTLC1400.test.js
@@ -144,7 +144,7 @@ contract("HTLC1400", ([issuer, investor1, investor2, investor3])=>{
             it("fetches the order details", async()=>{
 
                 const order = await htlc1400.checkOrder(orderID, erc1400.address)
-                order._recipient.should.be.equal(investor1, "it fetched the recipient of the order")
+                order._investor.should.be.equal(investor1, "it fetched the recipient of the order")
                 order._issuer.should.be.equal(issuer, "it fetched the issuer of the order")
                 order._amount.toString().should.be.equal(tokens(5).toString(),"it fetched the amount in the order")
                 order._expiration.toString().should.be.equal(expiration.toString(),"it fetched the expiration of the order")
