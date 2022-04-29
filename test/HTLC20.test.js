47116fa947bb21b111c90d7e3f0d7e1ee7dc60b2
komus-Israel <komolehinisrael@gmail.com>
2022-04-29 20:49:01 +0100

    imported expire, expired, stringToHex and hashSecret to the htlc20 test script

--- test/HTLC20.test.js
+++ test/HTLC20.test.js
@@ -2,9 +2,8 @@ require("chai")
     .use(require("chai-as-promised"))
     .should()
 
-const { ethers } = require("ethers");
 const moment = require("moment");
-const { ETHER_ADDRESS, tokens, swapState} = require("./helper.js")
+const { ETHER_ADDRESS, tokens, swapState, expire, expired, stringToHex, hashSecret} = require("./helper.js")
 
 //  connect to the smart contract
 
@@ -17,12 +16,10 @@ contract("HTLC20", ([issuer, investor1, investor2])=>{
     let htlc20 
     let erc20
     let secret_phrase = "anonymous"
-    let secretBytes32 = web3.utils.asciiToHex(secret_phrase)
-    let dataHex1 = web3.eth.abi.encodeParameter("bytes32", secretBytes32)
-    let secretHash = ethers.utils.sha256(dataHex1)
-    let orderID = web3.utils.asciiToHex("x23dvsdgd")
-    let expiration = new Date(moment().add(1, 'days').unix()).getTime()     // expiration will be present time + 1 day
-    let classA = web3.utils.asciiToHex("CLASS A")
+    let secretHash = hashSecret(secret_phrase).secretHash
+    let orderID = stringToHex("x23dvsdgd").hex
+    let expiration = expire(1)     // expiration will be present time + 1 day
+    let classA = stringToHex("CLASS A").hex
     let price = tokens(1000)                                                // price of the asset
     let amount = tokens(10)                                                 // quantity of asset to be issued
 
