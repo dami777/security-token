c3081c8ee83b15c7a90ae0e3e56d1def6a5ece32
komus-Israel <komolehinisrael@gmail.com>
2022-05-04 21:34:15 +0100

    require that the caller of the withdrawl function in the htlc20 contract is the issuer registered with that order

--- contracts/htlc/HTLC20.sol
+++ contracts/htlc/HTLC20.sol
@@ -66,7 +66,7 @@ contract HTLC20 {
 
     function issuerWithdrawal(bytes32 _swapID, bytes32 _secretKey, address _securityToken) external {
 
-        require(msg.sender == _owner, "invalid caller");
+        require(_orderSwap[_securityToken][_swapID]._issuer == msg.sender, "invalid caller");
         require(_swapState[_securityToken][_swapID] == OrderLibrary.SwapState.OPEN, "not opened");
         require(_orderSwap[_securityToken][_swapID]._funded == true, "not funded");
         OrderLibrary.OrderSwap memory _order = _orderSwap[_securityToken][_swapID];
