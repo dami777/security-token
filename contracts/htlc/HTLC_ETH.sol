7a56fbac9aa39d03b13e8a233283bde27ac0eb85
komus-Israel <komolehinisrael@gmail.com>
2022-05-06 16:06:49 +0100

    adjusted the dev comment present after the title

--- contracts/htlc/HTLC_ETH.sol
+++ contracts/htlc/HTLC_ETH.sol
@@ -1,5 +1,5 @@
 /// @title  HTLC to release ETH from investor to issuer
-/// @dev    Contract for DVP. Investor funds this contract with his ETH and gets the security token in exchange.
+/// @dev    Contract for DVP. Investors funds this contract with their ETH and they get the security token in exchange.
 
 pragma solidity 0.8.10;
 
