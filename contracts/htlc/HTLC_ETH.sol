ecfa906397fbacff8104bf53b66d8ca35ec62e0e
komus-Israel <komolehinisrael@gmail.com>
2022-05-11 09:34:04 +0100

    test that re entrancy attack fails on refund

--- contracts/htlc/HTLC_ETH.sol
+++ contracts/htlc/HTLC_ETH.sol
@@ -105,7 +105,7 @@ contract HTLC_ETH {
     /// @notice swapt state becomes `expired`
 
 
-    function refund(bytes32 _swapID, address _securityToken) external noReEntrancy {
+    function refund(bytes32 _swapID, address _securityToken) noReEntrancy external  {
 
         //require(_orderSwap[_securityToken][_swapID]._investor == msg.sender, "invalid caller");
         require(_swapState[_securityToken][_swapID] == OrderLibrary.SwapState.OPEN, "not opened");
