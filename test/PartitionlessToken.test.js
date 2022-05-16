4e6f86948196be9137677f3f05cf3c747f9ac8d8
komus-Israel <komolehinisrael@gmail.com>
2022-05-16 21:02:06 +0100

    defined data for issuers and onboared investors

--- test/PartitionlessToken.test.js
+++ test/PartitionlessToken.test.js
@@ -1,4 +1,4 @@
-const { stringToHex, setToken } = require("./helper")
+const { stringToHex, setToken, certificate } = require("./helper")
 
 const ERC1400 = artifacts.require("./ERC1400")
 
@@ -6,7 +6,7 @@ require("chai")
     .use(require("chai-as-promised"))
     .should()
 
-contract ("Partitionless Token", ([tanglAdministrator, reitAdministrator])=>{
+contract ("Partitionless Token", ([tanglAdministrator, reitAdministrator, investor_Dami, investor_Jeff])=>{
 
 
     let tanglSecurityToken
@@ -22,6 +22,48 @@ contract ("Partitionless Token", ([tanglAdministrator, reitAdministrator])=>{
     let reitTokenDetails = setToken("Real Estate Investment Trust", "REIT", 18, 0, [classA.hex,classB.hex])
 
 
+    /**
+     * Define the data of the issuers and onboarded investors
+     * These data will be used to generate certificate for issuance, transfer and redemption of tokens
+     */
+
+    let tanglAdministratorData = {
+            
+        firstName: "tangl administrator",
+        lastName: "tangl administrator",
+        location: "New Yoke, London",
+        walletAddress: tanglAdministrator
+
+    }
+
+
+
+    let reitAdministratorData = {
+            
+        firstName: "reit administrator",
+        lastName: "reit administrator",
+        location: "New Yoke, London",
+        walletAddress: reitAdministrator
+
+    }
+
+    let investorDamiData = {
+        firstName: "Dami",
+        lastName: "Ogunkeye",
+        location: "New Yoke, London",
+        walletAddress: investor_Dami
+    }
+
+
+    let investorJeffData = {
+
+        firstName: "Jeff",
+        lastName: "Chuka",
+        location: "New Yoke, London",
+        walletAddress: investor_Jeff
+
+    }
+
     beforeEach(async()=>{
 
         tanglSecurityToken = await ERC1400.new(tanglTokenDetails.name, tanglTokenDetails.symbol, tanglTokenDetails.decimal, {from: tanglAdministrator})
@@ -36,6 +78,12 @@ contract ("Partitionless Token", ([tanglAdministrator, reitAdministrator])=>{
             reitSecurityToken.address.should.not.be.equal("", "it has a contract contract")
             
         })
+
+    })
+
+    describe("issuance to partitionless token", ()=>{
+
+       
 
     })
 })
